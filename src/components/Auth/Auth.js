67c403efecd4f3f2120791d2b093e7c320d436dd
SergioV <sergioscode@gmail.com>
2022-04-19 04:02:26 -0700

    .

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Avatar, Button, Paper, Grid, Typography, Container } from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import useStyles from "./styles";
 
 function Auth() {
-    const state = null
+  const classes = useStyles();
+  const isSignup = false;
+
+  const hangleSubmit = () => {
+
+
+  }
+
   return (
-    <div>
+    <Container component="main" maxWidth="xs">
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography variant="h5">{isSignup ? "Sign Up" : "Sign In"}</Typography>
+        <form className={classes.form} onSubmit={handleSubmit} >
 
-    AUTH
-
-
-    </div>
-  )
+        </form>
+      </Paper>
+    </Container>
+  );
 }
 
-export default Auth
+export default Auth;
