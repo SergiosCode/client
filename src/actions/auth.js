7ae64279121f0833db8b208c8c61f73947d0da52
SergioV <sergioscode@gmail.com>
2022-05-01 14:16:00 -0700

    Completed the auth changes

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,11 +1,13 @@
 import { AUTH } from "../constants/actionTypes";
 import * as api from "../api/index.js";
-import { useDispatch } from "react-redux";
 
-const dispatch = useDispatch();
 
 export const signin = (formData, history) => async (dispatch) => {
   try {
+    const { data } = await api.signIn(formData);
+
+    dispatch({ type: AUTH, data });
+
     history.push("/");
   } catch (err) {
     console.log(err);
@@ -16,6 +18,11 @@ export const signin = (formData, history) => async (dispatch) => {
 
 export const signup = (formData, history) => async (dispatch) => {
   try {
+
+
+    const { data } = await api.signUp(formData);
+
+    dispatch({ type: AUTH, data });
     history.push("/");
   } catch (err) {
     console.log(err);
