3cc1f699635056c8467cd960d7005a9c3452afe1
SergioV <sergioscode@gmail.com>
2022-05-24 03:28:56 -0700

    Refactoring search bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import { Container } from "@material-ui/core";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
@@ -8,10 +8,13 @@ import Auth from "./components/Auth/Auth";
 const App = () => {
   return (
     <BrowserRouter>
-      <Container maxWidth="lg">
+      <Container maxWidth="xl">
         <Navbar />
         <Switch>
-          <Route path="/" exact component={Home} />
+          <Route path="/" exact component={() => <Redirect to='/posts'  />} />
+          <Route path ="/posts" component={Home} />
+          <Route path ="/posts/search" component={Home} />
+          <Route path ="/posts/:id " exact component={PostDetails} />
           <Route path="/auth" exact component={Auth} />
         </Switch>
       </Container>
