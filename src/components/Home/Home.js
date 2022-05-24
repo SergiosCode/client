3fea6c69824defaa8930db51a052bfc8b3f72477
SergioV <sergioscode@gmail.com>
2022-05-24 12:08:01 -0700

    Added a search bar

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,34 +1,65 @@
 import React, { useState, useEffect } from "react";
-import { Container, Grow, Grid, Paper } from "@material-ui/core";
+import { Container, Grow, Grid, Paper, AppBar, TextField, Button } from "@material-ui/core";
 import { useDispatch } from "react-redux";
 import { getPosts } from "../../actions/posts";
+import { useHistory, useLocation } from "react-router-dom";
+import ChipInput from "material-ui-chip-input";
 import useStyles from "./styles";
 import Pagination from "../Pagination";
 import Posts from "../Posts/Posts";
 import Form from "../Form/Form";
 
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
 function Home() {
   const [currentId, setCurrentId] = useState(null);
   const classes = useStyles();
   const dispatch = useDispatch();
+  const query = useQuery();
+  const history = useHistory();
+  const page = query.get("page") || 1;
+  const searchQuery = query.get("search") || "";
+
+  const [search, setSearch] = useState(" ");
 
   useEffect(() => {
     dispatch(getPosts());
   }, [currentId, dispatch]);
 
+  const handleKeyPress = (e) => {
+    if(e.keyCode === 13) {
+      history.push(`/posts?search=${search}`);
+    }
+  }
+
   return (
     <Grow in>
-      <Container>
+      <Container maxWidth="xl">
         <Grid
           container
-          className={classes.mainContainer}
+          className={classes.gridContainer}
           justifyContent="space-between"
           alignItems="stretch"
           spacing={3}>
-          <Grid item xs={12} sm={7}>
+          <Grid item xs={12} sm={6} md={9}>
             <Posts setCurrentId={setCurrentId} />
           </Grid>
-          <Grid item xs={12} sm={4}>
+          <Grid item xs={12} sm={6} md={3}>
+            <AppBar className={classes.appBarSearch} position="static" color="inherit">
+              <TextField
+                name="search"
+                variant="outlined"
+                label="Search Memories"
+                onKeyPress={handleKeyPress}
+                fullWidth
+                value={search}
+                onChange={(e) => {
+                  setSearch(e.target.value);
+                }}
+              />
+            </AppBar>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
             <Paper elevation={6}>
               <Pagination />
---
+++ src/components/PostDetails/PostDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PostDetails() {
+
+    console.log(`post`);
+  return (
+    <div>PostDetails</div>
+  )
+}
+
+export default PostDetails
